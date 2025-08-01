d8c63cd8ae3f27e10d405b4b38c930eccdec682e
jrohan-16 <81601825+jrohan-16@users.noreply.github.com>
2025-08-01 17:55:00 -0400

    Add Tailwind and static adapter setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+import '../app.css';
+import favicon from '$lib/assets/favicon.svg';
+
+let { children } = $props();
+</script>
+
+<svelte:head>
+        <link rel="icon" href={favicon} />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />
+</svelte:head>
+
+{@render children?.()}
