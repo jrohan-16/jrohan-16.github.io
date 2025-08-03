c44cb91c1bfeaddf3ef16d18925f24e58dc387e5
jrohan-16 <81601825+jrohan-16@users.noreply.github.com>
2025-08-03 16:53:23 -0700

    Create +layout.svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import '../app.css';
+  import favicon from '$lib/assets/favicon.svg';
+  import Nav from '$lib/components/Nav.svelte';
+  export let data: any;
+</script>
+
+<svelte:head>
+  <link rel="icon" href={favicon} />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+  <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />
+</svelte:head>
+
+<Nav />
+<slot />
