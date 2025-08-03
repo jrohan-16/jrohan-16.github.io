7bd1b595ce0f8adcd5ed530f4f63dfd34e2171f6
jrohan-16 <81601825+jrohan-16@users.noreply.github.com>
2025-08-02 19:30:01 -0700

    Add navigation links in root page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 <div class="p-6 bg-gray-bg">
   <nav class="mb-4">
-    Dashboard | Input | Compare | Export
-  </nav>
-  <ScenarioGrid />
-</div>
+  a        <a href="/" class="mr-2">Dashboard</a> |
+        <a href="/input" class="mx-2">Input</a> |
+        <a href="/compare" class="mx-2">Compare</a> |
+        <a href="/export" class="ml-2">Export</a>>
