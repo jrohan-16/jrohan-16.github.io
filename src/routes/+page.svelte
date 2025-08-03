76acf7472c92046e1290db134cf5aaa9d8502b51
jrohan-16 <81601825+jrohan-16@users.noreply.github.com>
2025-08-02 19:31:49 -0700

    Fix nav markup in root page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 <div class="p-6 bg-gray-bg">
   <nav class="mb-4">
-  a        <a href="/" class="mr-2">Dashboard</a> |
+ a        <a href="/" class="mr-2">Dashboard</a> |
         <a href="/input" class="mx-2">Input</a> |
         <a href="/compare" class="mx-2">Compare</a> |
         <a href="/export" class="ml-2">Export</a>>
