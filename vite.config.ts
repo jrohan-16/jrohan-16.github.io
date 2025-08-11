fe1d48cc83eba8fbe8f71bef31306c2b1909704e
jrohan-16 <81601825+jrohan-16@users.noreply.github.com>
2025-08-11 09:05:14 -0400

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
-
-const config: UserConfig = {
-  plugins: [sveltekit()],
-  server: { port: 5173 }
-};
-export default config;
