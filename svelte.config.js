4021476af35978b0ea74282ff5a4930dac207937
jrohan-16 <81601825+jrohan-16@users.noreply.github.com>
2025-12-14 19:08:16 -0800

    Add trailingSlash: always to kit config for GitHub Pages routing

--- svelte.config.js
+++ svelte.config.js
@@ -4,12 +4,13 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
-  kit: {
+    kit: {
     adapter: adapter({ pages: 'docs', assets: 'docs', fallback: 'index.html' }),
     alias: {
       $components: 'src/components',
       $lib: 'src/lib'
-    }
+    },
+    trailingSlash: 'always'
   }
 };
 
